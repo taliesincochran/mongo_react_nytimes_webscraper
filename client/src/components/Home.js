0aab289bdcd858dfd364bdf0b6f891de3ff21953
Taliesin <tdcochran2@gmail.com>
2018-02-02 11:48:14 -0500

    fixed image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,13 +3,13 @@ import Img from './Img'
 const Home = props => {
 	return(
 		<div>
-			<Img 
-				alt='New York Times 1914' 
-				className="background-image" 
-				src='https://upload.wikimedia.org/wikipedia/commons/1/17/Headline_of_the_New_York_Times_June-29-1914.jpg' 
+			<Img
+				alt='New York Times 1914'
+				className="background-image"
+				image='https://upload.wikimedia.org/wikipedia/commons/1/17/Headline_of_the_New_York_Times_June-29-1914.jpg' 
 				style={{width:'100%'}}
 			/>
-		</div> 
+		</div>
 	)
 }
 export default Home;
