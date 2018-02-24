cf367fb61cb09135746d0ece1c4aec622fae4167
Taliesin Cochran <tdcochran2@gmail.com>
2018-02-24 10:58:09 -0500

    Update Img.js

--- client/src/components/Img.js
+++ client/src/components/Img.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Img = props => {
 	return (
 		<div>
-			<img src={props.image} alt={props.alt} className={props.className} style={props.style} />
+			<img src={props.image || "http://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"} alt={props.alt} className={props.className} style={props.style} />
 		</div>
 	)
 }
